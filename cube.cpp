cd87b58fa0f4994b78159bcbd05f867cd4e25c7b
ChaithanyaKota <chaithanya.kota007@gmail.com>
2024-01-10 21:56:40 -0600

    initial commit

---
+++ cube.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h> 
+
+
+float A, B, C; 
+
+
+float calculateX(int i, int j, int k) { 
+    return ;
+}
+
+float calculateY(int i, int j, int k) { 
+    return;
+}
+
+float calculateZ(int i, int j, int k) { 
+    return;
+}
+
+int main() {
+
+
+    return 0;
+}
